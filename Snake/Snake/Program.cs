348a0abde7d12056f77ab3d62830d59460d9fe8f
Runli <ilnur.gazizov@phystech.edu>
2015-12-11 20:29:10 +0300

    add Line class

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -7,12 +7,10 @@ using System.Threading.Tasks;
 namespace Snake {
     class Program {
         static void Main(string[] args) {
-            List<Point> pList = new List<Point>();
-            pList.Add(new Point(1, 3, '*'));
-            pList.Add(new Point(4, 5, '#'));
-            foreach (Point p in pList) {
-                p.Draw();
-            }
+            HorizontalLine hLine = new HorizontalLine(5, 10, 2, '*');
+            hLine.Drow();
+            VerticalLine vLine = new VerticalLine(8, 2, 10, '#');
+            vLine.Drow();
             Console.ReadLine();
         }     
     }
