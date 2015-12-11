a7fcb99c8be9a3f872c69e21d36614a12831c8c4
Runli <ilnur.gazizov@phystech.edu>
2015-12-11 19:37:07 +0300

    add constructor

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -10,9 +10,16 @@ namespace Snake {
         public int y;
         public  char sym;
 
+        public Point() { }
+        public Point(int _x, int _y, char _sym){
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw() {
-            Console.SetCursorPosition(this.x, this.y);
-            Console.Write(this.sym);
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -7,16 +7,10 @@ using System.Threading.Tasks;
 namespace Snake {
     class Program {
         static void Main(string[] args) {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';           
+            Point p1 = new Point(1, 3, '*');           
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.sym = '#';
-            p2.x = 4;
-            p2.y = 5;
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
             Console.ReadLine();
         }     
